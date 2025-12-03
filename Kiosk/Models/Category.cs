8954b423c3442e179638466670c8e59455f9092d
Jakub <jakubD2002r@wp.pl>
2025-12-03 23:58:39 +0100

    test

--- Kiosk/Models/Category.cs
+++ Kiosk/Models/Category.cs
@@ -1,5 +1,8 @@
-﻿public class Category
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Category
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
 }
